4d65056b7d41f7e1971de2712f3c67a55210d10a
wq <1053567708@qq.com>
2021-12-12 17:25:51 -0800

    mongodb success connect

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,24 +1,27 @@
+const { query } = require("express");
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test1');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910126');
 
-const schema={
-    name:String,
-    age:Number,
-    health:String,
-    hooby:String,
+const schema = {
+    name: String,
+    age: Number,
+    health: String,
+    hooby: String,
 }
 const mydata = mongoose.model('cats1', schema);
 
-const kitty = new mydata({ name: 'Zildjian2' });
-kitty.save()
-const kitty1 = new mydata({ name: 'Zildjian4' });
-kitty1.save()
+// const kitty = new mydata({ name: 'Zildjian2' });
+// kitty.save()
+// const kitty1 = new mydata({ name: 'Zildjian5' });
+// kitty1.save()
 
 app.use('/', express.static('public'))
-app.get("/input",(req,res)=>{
+app.get("/input", (req, res) => {
     res.send(req.query)
     console.log(req.query)
+    const kitty = new mydata({ name: req.query.first,health:req.query.second});
+    kitty.save()
 })
 app.listen(10126)
